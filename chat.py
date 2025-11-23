0a251cb384e9e16cc8e4c2ec3b534d60cc75b834
danccoelho <coelhodaniel06087@gmail.com>
2025-11-23 17:13:41 -0300

    lógica do chat

--- chat.py
+++ chat.py
@@ -1,7 +1,5 @@
-import os
 import streamlit as st
 from groq import Groq
-
 
 SYSTEM_PROMPT = """
 Você é um Assistente Especialista em Machine Learning, Deep Learning, MLOps, LLMs e Explicabilidade de Modelos (XAI).
@@ -46,6 +44,17 @@ OBJETIVO FINAL:
 Ajudar o usuário a compreender profundamenteMachine Learning, Deep Learning, MLOps e LLMs, interpretar modelos, tomar melhores decisões técnicas e aprimorar soluções de IA com precisão, clareza e boas práticas.
 """
 
+def validar_api_key(api_key: str):
+    if not api_key or api_key.strip() == "":
+        return False, "API Key vazia. Insira uma para continuar."
+
+    try:
+        client = Groq(api_key=api_key)
+        client.models.list()    
+        return True, "API Key válida!"
+    except Exception:
+        return False, "API Key inválida ou não foi possível validar."
+
 
 st.set_page_config(
     page_icon= "⚡",
@@ -53,3 +62,82 @@ st.set_page_config(
     layout= "wide",
     initial_sidebar_state="expanded"
 )
+
+if "show_sidebar" not in st.session_state:
+    st.session_state.show_sidebar = True
+
+if "api_valida" not in st.session_state:
+    st.session_state.api_valida = False
+
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+
+with st.sidebar:
+
+  if st.session_state.show_sidebar:
+    st.title("⚙️ Configurações")
+    st.subheader("🔑 API")
+    groq_api_key = st.text_input(
+            "Groq API Key",
+            type="password",
+            placeholder="Digite sua API Key..."
+    )
+    
+    if groq_api_key:
+        valida, msg = validar_api_key(groq_api_key)
+        if valida:
+            st.success(msg)
+            st.session_state.api_valida = True
+        else:
+            st.error(msg)
+            st.session_state.api_valida = False
+    else:
+        st.warning("Digite sua API Key para continuar.")
+
+    st.subheader("🧹 Sessão")
+    if st.button("Limpar Conversa"):
+        st.session_state["messages"] = []
+        st.success("Conversa apagada!")
+
+    st.markdown("---")
+    st.caption("**AI Expert Chat** — Assistente especializado em Machine Learning, XAI e LLMs.")
+    st.caption("Desenvolvido por Daniel Coelho 🚀")
+
+
+st.title("IA Expert chat")
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if not st.session_state.api_valida:
+    st.warning("🔒 Insira sua API Key na barra lateral para liberar o chat.")
+    st.stop()
+
+client = Groq(api_key=groq_api_key)
+
+if prompt := st.chat_input():
+  st.session_state.messages.append({"role": "user", "content": prompt})
+
+  with st.chat_message("user"):
+    st.markdown(prompt)
+  
+  message_api_key = [{"role": "system", "content": SYSTEM_PROMPT}]
+  for msg in st.session_state.messages:
+    message_api_key.append(msg)
+
+  with st.chat_message("assistant"):
+    with st.spinner("⏳Pensando..."):
+            try:
+              chat_completion = client.chat.completions.create(
+                messages = message_api_key,
+                model = "openai/gpt-oss-20b",
+                temperature = 0.7,
+                max_tokens = 204,
+              )
+              response =  chat_completion.choices[0].message.content
+              st.markdown(response)
+              st.session_state.messages.append({"role": "assistant", "content": response})
+            except Exception as e:
+              st.error(f"Erro na API: {e}")
